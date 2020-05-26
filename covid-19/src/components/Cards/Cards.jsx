cb0dc476cd22c93a76771985e10274f0d87a9eae
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-26 18:20:40 +0530

    fix-bug_cards

--- covid-19/src/components/Cards/Cards.jsx
+++ covid-19/src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid  } from '@material-ui/core';
-import CountUp from 'react-countup';
+import CountUp from 'react-countup'; 
 import styles from './Cards.module.css';
 
 
@@ -15,12 +15,11 @@ const Cards = ( {data:{ confirmed, recovered, deaths, lastUpdate} }) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
-                            <countUp 
+                            <CountUp 
                               start = {0}
                               end={confirmed.value}
                               duration={2.5}
                               separator=","
-
                             />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
@@ -33,7 +32,7 @@ const Cards = ( {data:{ confirmed, recovered, deaths, lastUpdate} }) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
-                            <countUp 
+                            <CountUp 
                               start = {0}
                               end={recovered.value}
                               duration={2.5}
@@ -49,13 +48,12 @@ const Cards = ( {data:{ confirmed, recovered, deaths, lastUpdate} }) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
-                            <countUp 
+                            <CountUp 
                               start = {0}
                               end={deaths.value}
                               duration={2.5}
                               separator=","
                             />
-                        
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number Of deaths Cases of COVID-19</Typography>
