d1379a2ee6cce2c1ad7fa866e78c1df35b87fb49
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-26 17:07:28 +0530

    grid-card-componenet

--- covid-19/src/components/Cards/Cards.jsx
+++ covid-19/src/components/Cards/Cards.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
+import styles from './Cards.module.css';
+
 
 const Cards = (props) => {
     console.log(props);
 
     return(
         <div className={Styles.container}>
-            
+            <Grid container spacing={3} justify="center">
+                <Grid item component={card}>
+                    <
+                </Grid>
+            </Grid>
         </div>
     )
 }
