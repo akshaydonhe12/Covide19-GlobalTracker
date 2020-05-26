ee13ddfd26e3e8927df339462c3dd8feba148c3e
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-26 16:45:08 +0530

    error-modifieddata-fix

--- covid-19/src/api/index.js
+++ covid-19/src/api/index.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const FetchData = async () => {
+export const fetchdata = async () => {
     try{
-        const response = await axios.get(url);
-        return response;
+        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+
+        const modifiedData = { confirmed, recovered, deaths, lastUpdate,}
+
+        return modifiedData;
+
     } catch (error) {
 
     }
