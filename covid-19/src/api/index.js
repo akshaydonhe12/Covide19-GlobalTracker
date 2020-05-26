7de3042e67291ee34fb55170eb42957f41c71688
akshaydonhe12 <akshaydonhe02@gmail.com>
2020-05-26 16:29:14 +0530

    fetchdata_url

--- covid-19/src/api/index.js
+++ covid-19/src/api/index.js
@@ -1,3 +1,12 @@
 import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
+
+export const FetchData = async () => {
+    try{
+        const response = await axios.get(url);
+        return response;
+    } catch (error) {
+
+    }
+}
